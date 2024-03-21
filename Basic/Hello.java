8ad3aec2f72a83e8b307bfbc38bf51d596e5ee97
wcl-570 <160880807+wcl-570@users.noreply.github.com>
2024-03-21 20:26:00 +0800

    Update and rename Hello.java to Basic/Hello.java

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         // 程式進入點
-        System.out.println("Hello, I am Gay");
-        System.out.println("Hello, I am Lesbian");
+        System.out.println("Hello, I am Java");
     }
 }
